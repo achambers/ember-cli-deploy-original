8eb4f61f31a4b973be0b4a41f0ad8a30402198db
Josh Brandoff <josh@joshua.is>
2014-11-04 18:48:50 -0500

    Added tests for specifying git path.

--- tests/helpers/mock-nested-project.js
+++ tests/helpers/mock-nested-project.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var MockProject = function() {
-  this.root = process.cwd();
+  this.root = process.cwd() + '/tests/fixtures/fakeproject';
 };
 
 module.exports = MockProject;
--- tests/unit/utilities/git-utils-test.js
+++ tests/unit/utilities/git-utils-test.js
@@ -1,31 +1,88 @@
 'use strict';
 
-var assert      = require('../../helpers/assert');
-var GitUtils    = require('../../../lib/utilities/git-utils');
+var assert = require('../../helpers/assert');
+var GitUtils = require('../../../lib/utilities/git-utils');
 var MockProject = require('../../helpers/mock-project');
+var MockNestedProject = require('../../helpers/mock-nested-project');
 
 describe('git-utils', function() {
-  var subject;
+    describe('undefined-git-root', function() {
+        var subject;
+        console.log(process.cwd());
+        beforeEach(function() {
+            subject = new GitUtils({
+                project: new MockProject()
+            });
+        });
 
-  beforeEach(function() {
-    subject = new GitUtils({
-      project: new MockProject()
+        describe('#hash', function() {
+            it('returns the short SHA', function() {
+                var result = subject.hash();
+
+                assert.ok(/[0-9a-f]{10}/.test(result), 'Should not be able to return hash as HEAD should have no ref path');
+            });
+        });
+
+        describe('#branch', function() {
+            it('returns the branch name', function() {
+                var result = subject.branch();
+
+                assert.ok(/.+/.test(result), 'Should not be able to return hash as HEAD should have no ref path');
+            });
+        });
     });
-  });
 
-  describe('#hash', function() {
-    it('returns the short SHA', function() {
-      var result = subject.hash();
+    describe('unspecified-git-root', function() {
+        var subject;
 
-      assert.ok(/[0-9a-f]{10}/.test(result), 'Should not be able to return hash as HEAD should have no ref path');
+        beforeEach(function() {
+            subject = new GitUtils({
+                project: new MockProject(),
+                gitRoot: ''
+            });
+        });
+
+        describe('#hash', function() {
+            it('returns the short SHA', function() {
+                var result = subject.hash();
+
+                assert.ok(/[0-9a-f]{10}/.test(result), 'Should not be able to return hash as HEAD should have no ref path');
+            });
+        });
+
+        describe('#branch', function() {
+            it('returns the branch name', function() {
+                var result = subject.branch();
+
+                assert.ok(/.+/.test(result), 'Should not be able to return hash as HEAD should have no ref path');
+            });
+        });
     });
-  });
 
-  describe('#branch', function() {
-    it('returns the branch name', function() {
-      var result = subject.branch();
+    describe('specified-git-root', function() {
+        var subject;
 
-      assert.ok(/.+/.test(result), 'Should not be able to return hash as HEAD should have no ref path');
+        beforeEach(function() {
+            subject = new GitUtils({
+                project: new MockNestedProject(),
+                gitRoot: process.cwd()
+            });
+        });
+
+        describe('#hash', function() {
+            it('returns the short SHA', function() {
+                var result = subject.hash();
+
+                assert.ok(/[0-9a-f]{10}/.test(result), 'Should not be able to return hash as HEAD should have no ref path');
+            });
+        });
+
+        describe('#branch', function() {
+            it('returns the branch name', function() {
+                var result = subject.branch();
+
+                assert.ok(/.+/.test(result), 'Should not be able to return hash as HEAD should have no ref path');
+            });
+        });
     });
-  });
 });
