465627d8ab017d9a5bac17092ab27802914027bf
Aaron Chambers <achambers@gmail.com>
2014-11-08 08:12:27 +0000

    Now we retrieve the git hash/branch from a git command.
    Instead of inspecting the .git dir directly.  This means that the ember
    app can be a sub directory of a git repo

--- tests/unit/utilities/git-utils-test.js
+++ tests/unit/utilities/git-utils-test.js
@@ -5,85 +5,33 @@ var GitUtils = require('../../../lib/utilities/git-utils');
 var MockProject = require('../../helpers/mock-project');
 
 describe('git-utils', function() {
-    describe('undefined git root', function() {
-        var subject;
+  var subject;
 
-        beforeEach(function() {
-            subject = new GitUtils({
-                project: new MockProject()
-            });
-        });
+  beforeEach(function() {
+    subject = new GitUtils({
+      project: new MockProject()
+    });
+  });
 
-        describe('#hash', function() {
-            it('returns the short SHA', function() {
-                var result = subject.hash();
-
-                assert.ok(/[0-9a-f]{10}/.test(result), 'Should not be able to return hash as HEAD should have no ref path');
-            });
-        });
-
-        describe('#branch', function() {
-            it('returns the branch name', function() {
-                var result = subject.branch();
-
-                assert.ok(/.+/.test(result), 'Should not be able to return hash as HEAD should have no ref path');
-            });
+  describe('#hash', function() {
+    it('returns the short SHA', function() {
+      return subject.hash()
+        .then(function(hash) {
+          assert.ok(/[0-9a-f]{10}/.test(hash), 'Should return hash');
+        }, function() {
+          assert.ok(false, 'Should have returned hash');
         });
     });
+  });
 
-    describe('empty git root specified', function() {
-        var subject;
-
-        beforeEach(function() {
-            subject = new GitUtils({
-                project: new MockProject(),
-                gitRoot: ''
-            });
-        });
-
-        describe('#hash', function() {
-            it('returns the short SHA', function() {
-                var result = subject.hash();
-
-                assert.ok(/[0-9a-f]{10}/.test(result), 'Should not be able to return hash as HEAD should have no ref path');
-            });
-        });
-
-        describe('#branch', function() {
-            it('returns the branch name', function() {
-                var result = subject.branch();
-
-                assert.ok(/.+/.test(result), 'Should not be able to return hash as HEAD should have no ref path');
-            });
+  describe('#branch', function() {
+    it('returns the branch name', function() {
+      return subject.branch()
+        .then(function(branch) {
+          assert.ok(/.*/.test(branch), 'Should return branch');
+        }, function() {
+          assert.ok(false, 'Should have returned branch');
         });
     });
-
-    describe('specified-git-root', function() {
-        var subject;
-
-        beforeEach(function() {
-            subject = new GitUtils({
-                project: new MockProject({
-                    cwd: process.cwd() + '/tests'
-                }),
-                gitRoot: process.cwd()
-            });
-        });
-
-        describe('#hash', function() {
-            it('returns the short SHA', function() {
-                var result = subject.hash();
-
-                assert.ok(/[0-9a-f]{10}/.test(result), 'Should not be able to return hash as HEAD should have no ref path');
-            });
-        });
-
-        describe('#branch', function() {
-            it('returns the branch name', function() {
-                var result = subject.branch();
-
-                assert.ok(/.+/.test(result), 'Should not be able to return hash as HEAD should have no ref path');
-            });
-        });
-    });
+  });
 });
